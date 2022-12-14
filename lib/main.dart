f39d21636bc4de0ab066e6c9326c8419ad562089
reemswe <98765331+reemswe@users.noreply.github.com>
2022-12-14 18:18:51 +0300

    added firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'Devices/housesDevices.dart';
+import 'Devices/listOfDevices.dart';
+import 'Firebase/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //! Firebase
+  await Firebase.initializeApp(
+    // name: 'rashd',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -12,19 +24,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const devicesList(),
     );
   }
 }
