78d82f78207d25f8c7fdcfc3d186e1ae74168b2f
Haifa-saud <105865534+Haifa-saud@users.noreply.github.com>
2023-01-05 21:27:54 +0300

    list of house account preliminary UI

--- lib/list_of_houseAccounts.dart
+++ lib/list_of_houseAccounts.dart
@@ -1,9 +1,11 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_segmented_tab_control/animated_segmented_tab_control.dart';
+import 'package:rashd/dashboard.dart';
 
 import 'create_house_account.dart';
-import 'dashboard.dart';
 
 class ListOfHouseAccounts extends StatefulWidget {
   const ListOfHouseAccounts({super.key});
@@ -18,11 +20,15 @@ class _ListOfHouseAccountsState extends State<ListOfHouseAccounts> {
   void initState() {
     setState(() {
       getData();
-      getOwner();
-      getMember();
+
+      owner = getOwner();
+      member = getMember();
     });
     super.initState();
   }
+
+  Future? owner;
+  Future? member;
 
   List? houseOwner = [];
   List? houseMember = [];
@@ -104,57 +110,150 @@ class _ListOfHouseAccountsState extends State<ListOfHouseAccounts> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: FutureBuilder(
-            future: getData(),
-            builder: (context, snapshot) {
-              print('name:$name');
-              return Text(
-                '!مرحبًا $name',
-                textAlign: TextAlign.right,
-              );
-            }),
-        leading: const Text(''),
-        centerTitle: true,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.horizontal(
-            left: Radius.circular(40),
-            right: Radius.zero,
-          ),
-        ),
-        backgroundColor: const Color.fromARGB(255, 119, 201, 239),
-      ),
-      body: SingleChildScrollView(
-          child: ListView(padding: const EdgeInsets.all(20), children: [
-        Container(
-            padding: const EdgeInsets.fromLTRB(6, 12, 0, 12),
-            child: TextFormField(
-              // maxLength: 20,
-              readOnly: true,
-              textAlign: TextAlign.right,
-              decoration: InputDecoration(
-                hintText: 'قائمة منازلي',
-                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                border: InputBorder.none,
-                prefixIcon: IconButton(
-                  icon: const Icon(
-                      // Based on passwordVisible state choose the icon
-                      Icons.add),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CreateHouseAccount()),
-                    );
-                  },
-                ),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: FutureBuilder(
+                future: getData(),
+                builder: (context, snapshot) {
+                  print('name:$name');
+                  return Text(
+                    '!مرحبًا $name',
+                    textAlign: TextAlign.right,
+                  );
+                }),
+            leading: const Text(''),
+            centerTitle: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.horizontal(
+                left: Radius.circular(40),
+                right: Radius.zero,
               ),
-            )),
-      ])),
-      bottomNavigationBar: buildBottomNavigation(),
-    );
+            ),
+            backgroundColor: const Color.fromARGB(255, 119, 201, 239),
+          ),
+          body: SafeArea(
+              child: Stack(children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child:
+                  //     child: SingleChildScrollView(
+
+                  SegmentedTabControl(
+                // Customization of widget
+
+                backgroundColor: Colors.grey[100],
+                indicatorColor: Colors.grey[400],
+                tabTextColor: Colors.black45,
+                selectedTabTextColor: Colors.white,
+                squeezeIntensity: 2,
+                height: 45,
+
+                tabPadding: const EdgeInsets.symmetric(horizontal: 8),
+                textStyle: Theme.of(context).textTheme.bodyText1,
+                // Options for selection
+                // All specified values will override the [SegmentedTabControl] setting
+                tabs: const [
+                  SegmentTab(
+                    label: 'اشتراكاتي',
+                  ),
+                  SegmentTab(
+                    label: 'منازلي',
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: TabBarView(
+                    physics: const BouncingScrollPhysics(),
+                    children: [
+                      FutureBuilder(
+                        future: member,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return const Text(
+                              "Something went wrong",
+                            );
+                          }
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            houseMember = snapshot.data as List;
+                            print(houseMember);
+
+                            return buildItems(houseMember);
+                          }
+                          return const Center(
+                              child: CircularProgressIndicator());
+                          // membersList.removeAt(0);
+                          // print(membersList);
+                          // return buildItems(membersList);
+                        },
+                      ),
+                      FutureBuilder(
+                        future: owner,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return const Text(
+                              "Something went wrong",
+                            );
+                          }
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            houseOwner = snapshot.data as List;
+                            print(houseOwner);
+                            return buildItems(houseOwner);
+                          }
+                          return const Center(
+                              child: CircularProgressIndicator());
+                          // membersList.removeAt(0);
+                          // print(membersList);
+                          // return buildItems(membersList);
+                        },
+                      ),
+                    ])
+                // padding: const EdgeInsets.fromLTRB(6, 12, 0, 12),
+                )
+          ])),
+          bottomNavigationBar: buildBottomNavigation(),
+        ));
   }
+
+  Widget buildItems(dataList) => ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: dataList.length,
+      // separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+            margin: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+            decoration: BoxDecoration(
+              // border: Border.all(color: Color(0xff940D5A)),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(13.0),
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0, 6.0),
+                  blurRadius: 7.0,
+                ),
+              ],
+            ),
+            child: ListTile(
+                trailing: Text(
+                  dataList[index][0]["houseName"],
+                ),
+                leading: const Icon(Icons.arrow_back_ios),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => dashboard(
+                              dashId: dataList[index][0]["dashboardID"],
+                            )),
+                  );
+                }));
+      });
 
   Widget buildBottomNavigation() {
     return BottomNavyBar(
