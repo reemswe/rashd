53f70c0eb99c994ba57a1d5c93928badaa46ce2d
reemswe <98765331+reemswe@users.noreply.github.com>
2023-04-13 17:49:15 +0300

    fix error

--- lib/HouseAccount/list_of_houseAccounts.dart
+++ lib/HouseAccount/list_of_houseAccounts.dart
@@ -129,7 +129,7 @@ class _ListOfHouseAccountsState extends State<ListOfHouseAccounts>
 
   void rebuildPage() {
     setState(() {
-      getData();
+      getUsername();
       getOwner();
       getMember();
     });
